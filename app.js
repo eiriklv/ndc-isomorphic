bfc88cb661a891f6975d255f69c1220e82d4f24b
Eirik Vullum <evullum@gmail.com>
2015-06-09 13:44:15 +0200

    use process.env.PORT as default

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', require('./render/server'));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
